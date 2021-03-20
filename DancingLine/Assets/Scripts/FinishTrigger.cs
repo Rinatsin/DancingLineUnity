eb29ce1837441279a77d8d29acbe292a1b580f24
Rinatsin <rinatsin@gmail.com>
2021-03-21 00:15:17 +0300

    Add to project

---
+++ DancingLine/Assets/Scripts/FinishTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Finish");
+            other.GetComponent<Movement>().Speed = 0.1f;
+            other.GetComponent<Animator>().SetBool("isIdle", true);
+        }
+    }
+}
